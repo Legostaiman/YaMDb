24d8e7bd7ad4ccb795365c2a8820537ba25623d1
Andrey <twinka4ik174@mail.ru>
2021-01-12 20:42:00 +0500

    Add Comment Model

--- api/models.py
+++ api/models.py
@@ -37,3 +37,25 @@ class Review(models.Model):
         )
     )
     count = results.objects.count()
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+
+    text = models.TextField()
+
+    created = models.DateTimeField(
+        'Дата добавления',
+        auto_now_add=True,
+        db_index=True,
+    )
